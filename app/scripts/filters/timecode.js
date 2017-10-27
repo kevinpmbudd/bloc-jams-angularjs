4599782e1fba6e1e538d6fb7baf21935bfef7b64
kevinpmbudd <kevin@kevinbudd.com>
2017-10-26 21:38:50 -0500

    assignment 11 filters: completed

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,25 +1,7 @@
 (function() {
   function timecode() {
-    return function(secondsUnfiltered) {
-        let seconds = Number.parseFloat(secondsUnfiltered);
-
-        if (Number.isNaN(seconds)) {
-          return '-:--';
-        }
-
-        let wholeSeconds = Math.floor(seconds);
-        let minutes = Math.floor(wholeSeconds / 60);
-        let remainingSeconds = wholeSeconds % 60;
-
-        let output = `${minutes}:`;
-
-        if (remainingSeconds < 10) {
-          output += '0';
-        }
-
-        output += remainingSeconds;
-
-        return output;
+    return function(seconds) {
+        return buzz.toTimer(seconds);
     };
   }
 
